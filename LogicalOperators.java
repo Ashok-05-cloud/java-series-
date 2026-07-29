d17059dbcf1f53e695a070728731ef60ba5e16b8
B Ashok <ashokkumarnaidu548@gmail.com>
2026-07-29 22:07:52 +0530

    Add LogicalOperators class with boolean examples

---
+++ LogicalOperators.java
@@ -0,0 +1,11 @@
+public class LogicalOperators {
+    public static void main(String[] args) {
+
+        boolean java = true;
+        boolean dsa = false;
+
+        System.out.println(java && dsa);
+        System.out.println(java || dsa);
+        System.out.println(!java);
+    }
+}
