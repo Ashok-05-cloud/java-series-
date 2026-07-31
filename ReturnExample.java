80dd0e94b23a01caf5019ebb6d884a63979d5c44
B Ashok <ashokkumarnaidu548@gmail.com>
2026-07-31 18:35:06 +0530

    Add ReturnExample class with square method

---
+++ ReturnExample.java
@@ -0,0 +1,12 @@
+public class ReturnExample {
+
+    static int square(int n) {
+        return n * n;
+    }
+
+    public static void main(String[] args) {
+
+        int result = square(6);
+        System.out.println(result);
+    }
+}
