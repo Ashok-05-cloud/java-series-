9c061c9c3f7432caf5feee967c2b9df0cee97194
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-02 10:34:58 +0530

    Create StringsBasics.java

---
+++ StringsBasics.java
@@ -0,0 +1,12 @@
+public class StringBasics {
+    public static void main(String[] args) {
+
+        String name = "Ashok";
+
+        System.out.println("Name: " + name);
+        System.out.println("Length: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+        System.out.println("First Character: " + name.charAt(0));
+    }
+}
