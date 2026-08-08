4e2148ae3795fdb32e87ddc8b8cba6619cf245a6
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-08 21:30:21 +0530

    Create multiple catch blocks.java

---
+++ exceptional-handling/multiple catch blocks.java
@@ -0,0 +1,19 @@
+public class MultipleCatch {
+
+    public static void main(String[] args) {
+
+        try {
+            int[] arr = new int[3];
+            arr[5] = 10;
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception");
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array Index Out Of Bounds");
+        }
+        catch (Exception e) {
+            System.out.println("Some Other Exception");
+        }
+    }
+}
