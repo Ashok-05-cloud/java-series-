68930928922a01bd98b4ddacdbeaf53014f7eba9
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-09 14:14:00 +0530

    Add SearchElements class to check language presence

---
+++ collections/searchingelementsarray.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class SearchElements {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> languages = new ArrayList<>();
+
+        languages.add("Java");
+        languages.add("Python");
+
+        System.out.println(languages.contains("Java"));
+    }
+}
