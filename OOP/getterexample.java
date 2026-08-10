372a0dd7344785751020405698636eea3b920cf7
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-10 15:21:10 +0530

    Add Student class with getter for name

---
+++ OOP/getterexample.java
@@ -0,0 +1,15 @@
+public class Student {
+
+    private String name = "Ashok";
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+
+        Student s = new Student();
+
+        System.out.println(s.getName());
+    }
+}
