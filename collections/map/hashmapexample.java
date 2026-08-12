790d518e943268f3c7a10413abb4285e6b17a662
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-12 14:37:32 +0530

    Create hashmapexample.java

---
+++ collections/map/hashmapexample.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class HashMapExample {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> students = new HashMap<>();
+
+        students.put(101, "Ashok");
+        students.put(102, "Rahul");
+        students.put(103, "Kiran");
+
+        System.out.println(students);
+    }
+}
