8548c8460b27c43e6749b9106ce7da23aa8f5d9b
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-12 14:57:41 +0530

    Create variables.java

---
+++ java-basics/variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        String name = "Ashok";
+        int age = 21;
+        double cgpa = 9.0;
+        char grade = 'A';
+        boolean student = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + student);
+    }
+}
