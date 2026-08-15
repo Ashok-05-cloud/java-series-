5336ee2f2f9fe94ea5aea1efe2fe6ee8321ef729
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-15 13:09:36 +0530

    Create vectorexample.java

---
+++ DSA/vectorexample.java
@@ -0,0 +1,19 @@
+import java.util.Vector;
+
+public class VectorExample {
+
+    public static void main(String[] args) {
+
+        Vector<Integer> numbers = new Vector<>();
+
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        System.out.println(numbers);
+
+        numbers.remove(1);
+
+        System.out.println(numbers);
+    }
+}
