494bd162cd8c436ba46dab24713ad6d3bb49db0e
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-16 12:14:55 +0530

    Create BufferReaderexample.java

---
+++ FILE HANDLING/BufferReaderexample.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderExample {
+
+    public static void main(String[] args) {
+
+        try (
+            BufferedReader reader =
+                new BufferedReader(
+                    new FileReader("students.txt")
+                )
+        ) {
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
+    }
+}
