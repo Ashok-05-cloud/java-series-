25cc018a7a0c965526633340dde90dcca0dd7124
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-18 18:33:07 +0530

    Create consumer example .java

---
+++ java8 features/functional interface/consumer example .java
@@ -0,0 +1,12 @@
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+
+        Consumer<String> printName =
+                name -> System.out.println(name);
+
+        printName.accept("Ashok");
+    }
+}
