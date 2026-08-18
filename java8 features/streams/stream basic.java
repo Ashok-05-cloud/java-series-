4dfdc73ed84150d2e3b259f7050a6518a9f2871f
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-18 18:36:30 +0530

    Create streams/stream basic.java

---
+++ java8 features/streams/stream basic.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamBasic {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers =
+                Arrays.asList(10, 20, 30, 40);
+
+        numbers.stream()
+               .forEach(System.out::println);
+    }
+}
