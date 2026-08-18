140afa8a75bef8ff5be7d64d07af6f31f46fb817
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-18 18:28:47 +0530

    Create java8 features/lambda/lambda basics .java

---
+++ java8 features/lambda/lambda basics .java
@@ -0,0 +1,11 @@
+public class LambdaBasic {
+
+    public static void main(String[] args) {
+
+        Runnable r = () -> {
+            System.out.println("Hello from Lambda");
+        };
+
+        r.run();
+    }
+}
