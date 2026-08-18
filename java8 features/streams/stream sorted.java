29ed0fe9211a2c0e93787e56e8d1e8d20fcdba13
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-18 18:38:13 +0530

    Create stream sorted.java

---
+++ java8 features/streams/stream sorted.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamSorted {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers =
+                Arrays.asList(50, 10, 40, 20, 30);
+
+        numbers.stream()
+               .sorted()
+               .forEach(System.out::println);
+    }
+}
