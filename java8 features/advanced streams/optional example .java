c56ca32c3775f01ad76d51e066b7bf9fdd58a230
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-19 14:41:59 +0530

    Create optional example .java

---
+++ java8 features/advanced streams/optional example .java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class OptionalExample {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers =
+                Arrays.asList(10, 20, 30);
+
+        Optional<Integer> result =
+                numbers.stream()
+                       .filter(n -> n > 100)
+                       .findFirst();
+
+        System.out.println(
+            result.orElse(-1)
+        );
+    }
+}
