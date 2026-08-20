b25d1511e17204c0ff32ef9b35269b1c534985ce
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-20 19:08:09 +0530

    Create throws example .java

---
+++ exceptional-handling/Basics/throws example .java
@@ -0,0 +1,22 @@
+public class ThrowsExample {
+
+    static void divide(int a, int b)
+            throws ArithmeticException {
+
+        System.out.println(a / b);
+    }
+
+    public static void main(String[] args) {
+
+        try {
+
+            divide(10, 0);
+
+        } catch (ArithmeticException e) {
+
+            System.out.println(
+                "Cannot divide by zero."
+            );
+        }
+    }
+}
