326d1032cb206d5f4c5a9bdbefc29e448568ea9d
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-20 19:05:52 +0530

    Create exceptional example .java

---
+++ exceptional-handling/Basics/exceptional example .java
@@ -0,0 +1,18 @@
+public class ExceptionExample {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {10, 20, 30};
+
+        try {
+
+            System.out.println(numbers[5]);
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+
+            System.out.println(
+                "Invalid array index."
+            );
+        }
+    }
+}
