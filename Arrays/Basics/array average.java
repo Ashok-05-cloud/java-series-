15b61965561e558bebb5c2d55d1025e376f1ca8e
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-22 20:41:13 +0530

    Create array average.java

---
+++ Arrays/Basics/array average.java
@@ -0,0 +1,22 @@
+public class ArrayAverage {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {
+            10, 20, 30, 40, 50
+        };
+
+        int sum = 0;
+
+        for (int number : numbers) {
+            sum += number;
+        }
+
+        double average =
+                (double) sum / numbers.length;
+
+        System.out.println(
+            "Average = " + average
+        );
+    }
+}
