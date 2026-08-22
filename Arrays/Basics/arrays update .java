f2492d55bfcbe15435c92753254a0def980834f3
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-22 20:40:31 +0530

    Create arrays update .java

---
+++ Arrays/Basics/arrays update .java
@@ -0,0 +1,15 @@
+public class ArrayUpdate {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {
+            10, 20, 30, 40, 50
+        };
+
+        numbers[2] = 100;
+
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
