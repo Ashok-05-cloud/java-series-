7f63418ebac250982b2cbd826d046b2505e84428
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-23 18:32:35 +0530

    Create remove spaces .java

---
+++ STRINGS/Basic problems/remove spaces .java
@@ -0,0 +1,12 @@
+public class RemoveSpaces {
+
+    public static void main(String[] args) {
+
+        String text = "Java is easy";
+
+        String result =
+                text.replace(" ", "");
+
+        System.out.println(result);
+    }
+}
