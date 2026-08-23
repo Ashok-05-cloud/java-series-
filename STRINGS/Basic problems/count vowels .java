5000a8ea20a322900a4ad4210a55786df6d876fb
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-23 18:31:31 +0530

    Create Basic problems/count vowels .java

---
+++ STRINGS/Basic problems/count vowels .java
@@ -0,0 +1,32 @@
+public class CountVowels {
+
+    public static void main(String[] args) {
+
+        String text = "Hello Java";
+
+        int count = 0;
+
+        for (int i = 0;
+             i < text.length();
+             i++) {
+
+            char ch =
+                    Character.toLowerCase(
+                        text.charAt(i)
+                    );
+
+            if (ch == 'a' ||
+                ch == 'e' ||
+                ch == 'i' ||
+                ch == 'o' ||
+                ch == 'u') {
+
+                count++;
+            }
+        }
+
+        System.out.println(
+            "Vowels = " + count
+        );
+    }
+}
