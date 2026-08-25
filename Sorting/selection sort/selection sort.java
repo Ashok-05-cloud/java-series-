69ef733d1a2287d4f2ebf14208912d7bee9100e0
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-25 19:16:16 +0530

    Create selection sort/selection sort.java

---
+++ Sorting/selection sort/selection sort.java
@@ -0,0 +1,39 @@
+public class SelectionSort {
+
+    public static void sort(int[] arr) {
+
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[j] < arr[minIndex]) {
+
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[i];
+
+            arr[i] = arr[minIndex];
+
+            arr[minIndex] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {
+            5, 3, 8, 1, 2
+        };
+
+        sort(arr);
+
+        for (int number : arr) {
+            System.out.print(number + " ");
+        }
+    }
+}
