fd4d40c9aa626c81c8aeecb9f89e2651a4f8caed
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-25 19:16:51 +0530

    Create Insertion Sort/insertion Sort.java

---
+++ Sorting/Insertion Sort/insertion Sort.java
@@ -0,0 +1,34 @@
+public class InsertionSort {
+
+    public static void sort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+
+                arr[j + 1] = arr[j];
+
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {
+            5, 3, 8, 1, 2
+        };
+
+        sort(arr);
+
+        for (int number : arr) {
+            System.out.print(number + " ");
+        }
+    }
+}
