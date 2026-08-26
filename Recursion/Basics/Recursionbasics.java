e47c735c98506ff5c31f4e6746677e6ff8db846e
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-26 18:26:10 +0530

    Create Recursion/Basics/Recursionbasics.java

---
+++ Recursion/Basics/Recursionbasics.java
@@ -0,0 +1,18 @@
+public class RecursionBasics {
+
+    static void hello(int n) {
+
+        if (n == 0) {
+            return;
+        }
+
+        System.out.println("Hello");
+
+        hello(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        hello(5);
+    }
+}
