8cf9a695f6bd46c476533021d147f1ba050733b5
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-27 19:15:09 +0530

    Create Tower of hanoi .java

---
+++ Advanced recursion backtracking/practice/Tower of hanoi .java
@@ -0,0 +1,38 @@
+public class TowerOfHanoi {
+
+    static void solve(
+            int n,
+            char source,
+            char helper,
+            char destination) {
+
+        if (n == 0) {
+            return;
+        }
+
+        solve(
+            n - 1,
+            source,
+            destination,
+            helper
+        );
+
+        System.out.println(
+            "Move disk " + n +
+            " from " + source +
+            " to " + destination
+        );
+
+        solve(
+            n - 1,
+            helper,
+            source,
+            destination
+        );
+    }
+
+    public static void main(String[] args) {
+
+        solve(3, 'A', 'B', 'C');
+    }
+}
