4b5dbd2a906618b2002a17f45012c83b5613a00f
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-27 19:14:22 +0530

    Create practice/generate binary strings .java

---
+++ Advanced recursion backtracking/practice/generate binary strings .java
@@ -0,0 +1,27 @@
+public class GenerateBinaryStrings {
+
+    static void generate(
+            int n,
+            String current) {
+
+        if (current.length() == n) {
+            System.out.println(current);
+            return;
+        }
+
+        generate(
+            n,
+            current + "0"
+        );
+
+        generate(
+            n,
+            current + "1"
+        );
+    }
+
+    public static void main(String[] args) {
+
+        generate(3, "");
+    }
+}
