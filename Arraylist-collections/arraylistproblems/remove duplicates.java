3f9396b7c2e3a8fb8c6a6dde766f7d8fbfb7d111
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-28 23:01:59 +0530

    Create remove duplicates.java

---
+++ Arraylist-collections/arraylistproblems/remove duplicates.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers =
+            new ArrayList<>();
+
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(3);
+        numbers.add(4);
+
+        ArrayList<Integer> unique =
+            new ArrayList<>();
+
+        for (int number : numbers) {
+
+            if (!unique.contains(number)) {
+                unique.add(number);
+            }
+        }
+
+        System.out.println(unique);
+    }
+}
