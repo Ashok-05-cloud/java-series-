7076af41b54a14712cd58068b7827f9dfe5987db
B Ashok <ashokkumarnaidu548@gmail.com>
2026-08-28 22:56:09 +0530

    Create Arraylist-collections/array list basics /createarraylist.java

---
+++ Arraylist-collections/array list basics /createarraylist.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class CreateArrayList {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers =
+            new ArrayList<>();
+
+        System.out.println(numbers);
+    }
+}
